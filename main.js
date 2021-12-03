587362e827d3f32fb5f46e458a9bfc4e5554ead0
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-12-03 15:33:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+noseX = 0;
+noseY =0;
+
+leftWristX = 0;
+rightWristX = 0;
+
+difference = 0;
+
 function preload(){
 
 }
@@ -15,6 +23,10 @@ function setup(){
 
 function draw(){
     background("#969A97");
+    document.getElementById("square_sides").innerHTML = "height and width of the square will be = " + difference + " px";
+    fill('F90093');
+    stroke('F90093');
+    square(noseX , noseY , difference);
 }
 
 function modelLoaded(){
@@ -24,5 +36,16 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("left wrist X = " + leftWristX + " right wirst X = " + rightWristX + " differnce = " + difference);
+
     }
 }
